c33ac829ec89c624a7ae2dd47cbe3f31d1435c31
yasaichi <yasaichi@users.noreply.github.com>
2015-10-22 01:00:20 +0900

    Implement Chat class

---
+++ app/javascripts/chat.js
@@ -0,0 +1,38 @@
+class Chat {
+  constructor(socket) {
+    this.socket = socket;
+  }
+
+  sendMessage(room, text) {
+    this.socket.emit('message', {
+      room: room,
+      text: text
+    });
+  }
+
+  changeRoom(room) {
+    this.socket.emit('join', {
+      newRoom: room
+    });
+  }
+
+  processCommand(command) {
+    let message;
+    let words = command.split(' ');
+    const type = words.shift().substring(1).toLowerCase();
+
+    switch(type) {
+      case 'join':
+        this.changeRoom(words.join(' '));
+        break;
+      case 'nick':
+        this.socket.emit('nameAttempt', words.join(' '));
+        break;
+      default:
+        message = 'Unrecognized command.';
+        break;
+    }
+
+    return message;
+  }
+}

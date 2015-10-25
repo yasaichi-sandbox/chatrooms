e5dacad9d786b7d0e1e9687cdd2e8df630109a88
yasaichi <yasaichi@users.noreply.github.com>
2015-10-25 19:23:45 +0900

    add some tasks to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,49 @@
 'use strict';
 
 const gulp = require('gulp');
+const gutil = require('gulp-util');
+const uglify = require('gulp-uglify');
+const browserify = require('browserify');
+const watchify = require('watchify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const babelify = require('babelify');
 const jade = require('gulp-jade');
 const stylus = require('gulp-stylus');
 const minifyCss = require('gulp-minify-css');
 const sourcemaps = require('gulp-sourcemaps');
+
+const browserifyOptions = {
+  entries: ['./app/javascripts/chat.js', './app/javascripts/chat_ui.js'],
+  transform: [babelify],
+  debug: true
+};
+
+function bundleScriptsBy(bundler) {
+  bundler.bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/javascripts'));
+}
+
+gulp.task('browserify', () => {
+  const bundler = browserify(browserifyOptions);
+  bundleScriptsBy(bundler);
+});
+
+gulp.task('watchify', () => {
+  const options = Object.assign({}, watchify.args, browserifyOptions);
+  const bundler = watchify(browserify(options));
+
+  bundler.on('log', gutil.log);
+  bundler.on('update', () => bundleScriptsBy(bundler));
+
+  bundleScriptsBy(bundler);
+});
 
 gulp.task('jade', () => {
   gulp.src('./app/index.jade')
@@ -24,10 +63,11 @@ gulp.task('stylus', () => {
     .pipe(gulp.dest('./public/stylesheets'));
 });
 
-gulp.task('watch', ['jade', 'stylus'], () => {
+gulp.task('build', ['browserify', 'jade', 'stylus']);
+
+gulp.task('watch', ['watchify', 'jade', 'stylus'], () => {
   gulp.watch('./app/**/*.jade', ['jade']);
   gulp.watch('./app/stylesheets/*.styl', ['stylus']);
 });
 
-gulp.task('build', ['jade', 'stylus']);
 gulp.task('default', ['watch']);
